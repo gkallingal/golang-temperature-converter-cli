8468a6843372c7e9c7a9ae55b4a42eb315be8171
gkallingal <george.kallingal@sony.com>
2020-11-09 07:21:59 -0800

    Read Current Temp

--- main.go
+++ main.go
@@ -25,7 +25,10 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err = fmt.Scanln(originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
 		if shouldConvertAgain != "Y" {
